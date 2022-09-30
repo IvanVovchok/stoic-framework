f437cc880e343c628445c307d3145d839db321f6
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 01:07:08 +0300

    feat: implement routes; add error handling

--- web/front.php
+++ web/front.php
@@ -4,33 +4,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$response = new Response();
-$routes = new RouteCollection();
+$routes = include __DIR__.'/../src/app.php';
 
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$map = [
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-];
-
-$path = $request->getPathInfo();
-
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
 
     $response = new Response(ob_get_clean());
-} else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
