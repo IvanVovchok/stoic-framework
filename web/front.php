d7eef9376eb48e6a098111f58331d869f330d6a8
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 20:01:00 +0300

    feat: add GA code listener

--- web/front.php
+++ web/front.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
@@ -13,10 +14,24 @@ $routes = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response', function (Stoic\ResponseEvent $event) {
+    $response = $event->getResponse();
+
+    if ($response->isRedirection()
+        || ($response->headers->has('Content-Type') && !str_contains($response->headers->get('Content-Type'), 'html'))
+        || 'html' !== $event->getRequest()->getRequestFormat()
+    ) {
+        return;
+    }
+
+    $response->setContent($response->getContent().'GA CODE');
+});
+
 $controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-$framework = new Stoic\Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Stoic\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 $response = $framework->handle($request);
 
 $response->send();
