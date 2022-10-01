102776696adc2811aacb3169053bac65932716fe
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 19:38:20 +0300

    feat: add getters for ResponseEvent class

--- src/Stoic/ResponseEvent.php
+++ src/Stoic/ResponseEvent.php
@@ -2,7 +2,28 @@
 
 namespace Stoic;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\EventDispatcher\Event;
+
 class ResponseEvent
 {
+    private Request $request;
+    private Response $response;
 
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
 }
