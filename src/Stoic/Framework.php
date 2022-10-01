aa9aa63f4dd23451728a0c933170db551df879cd
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 17:48:51 +0300

    feat: add framework class properties & constructor

--- src/Stoic/Framework.php
+++ src/Stoic/Framework.php
@@ -2,8 +2,27 @@
 
 namespace Stoic;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
 class Framework
 {
+    private UrlMatcher $matcher;
+    private ControllerResolver $controllerResolver;
+    private ArgumentResolver $argumentResolver;
 
-
+    public function __construct(
+        UrlMatcher $matcher,
+        ControllerResolver $controllerResolver,
+        ArgumentResolver $argumentResolver
+    )
+    {
+        $this->matcher = $matcher;
+        $this->controllerResolver = $controllerResolver;
+        $this->argumentResolver = $argumentResolver;
+    }
 }
