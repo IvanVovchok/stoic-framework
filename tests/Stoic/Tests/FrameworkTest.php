8d9ba97c1baaa8f2fcfd206fdba5b5d2db96b26e
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 19:17:43 +0300

    fe

--- tests/Stoic/Tests/FrameworkTest.php
+++ tests/Stoic/Tests/FrameworkTest.php
@@ -2,16 +2,51 @@
 
 namespace Stoic\Tests;
 
+use Calendar\Controller\LeapYearController;
+use PhpParser\Node\Arg;
 use PHPUnit\Framework\TestCase;
 use Stoic\Framework;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class FrameworkTest extends TestCase
 {
+    public function testControllerResponse()
+    {
+        $matcher = $this->createMock(Routing\Matcher\UrlMatcherInterface::class);
+
+        $matcher
+            ->expects($this->once())
+            ->method('match')
+            ->will($this->returnValue([
+                '_route' => 'is_leap_year/{year}',
+                'year' => '2000',
+                '_controller' => [new LeapYearController(), 'index']
+            ]))
+        ;
+
+        $matcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->createMock(Routing\RequestContext::class)));
+        ;
+
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $framework = new Framework($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('Yep, this is a leap year!', $response->getContent());
+    }
+
     public function testNotFoundHandling()
     {
         $framework = $this->getFrameworkForException(new ResourceNotFoundException());
