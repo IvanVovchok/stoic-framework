ae9921d79ff1b04c34b9c43277590d76eeeb4a64
Ivan Vovchok <elvovchok@gmail.com>
2022-10-01 19:33:36 +0300

    feat: add event dispatcher to framework class

--- src/Stoic/Framework.php
+++ src/Stoic/Framework.php
@@ -2,25 +2,30 @@
 
 namespace Stoic;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use function PHPUnit\Framework\returnArgument;
 
 class Framework
 {
+    private EventDispatcher $dispatcher;
     private UrlMatcherInterface $matcher;
     private ControllerResolverInterface $controllerResolver;
     private ArgumentResolverInterface $argumentResolver;
 
     public function __construct(
+        EventDispatcher $dispatcher,
         UrlMatcherInterface $matcher,
         ControllerResolverInterface $controllerResolver,
         ArgumentResolverInterface $argumentResolver
     )
     {
+        $this->dispatcher = $dispatcher;
         $this->matcher = $matcher;
         $this->controllerResolver = $controllerResolver;
         $this->argumentResolver = $argumentResolver;
