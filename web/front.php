8b954496608519276294f47588c3f8237b2b1588
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 14:28:45 +0300

    feat: add custom listeners to container

--- web/front.php
+++ web/front.php
@@ -3,11 +3,18 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Stoic\StringResponseListener;
+use Symfony\Component\DependencyInjection\Reference;
 
 $routes = include __DIR__.'/../src/app.php';
 $container = include __DIR__.'/../src/container.php';
 
 $request = Request::createFromGlobals();
+
+$container->register('listener.string_response', StringResponseListener::class);
+$container->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', [new Reference('listener.string_response')])
+;
 
 $response = $container->get('framework')->handle($request);
 
