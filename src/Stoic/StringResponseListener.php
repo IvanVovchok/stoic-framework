6af67bccdfe782670ba1df5c52753693f45cbc7c
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 13:19:13 +0300

    feat: add StringResponseListener

--- src/Stoic/StringResponseListener.php
+++ src/Stoic/StringResponseListener.php
@@ -2,7 +2,23 @@
 
 namespace Stoic;
 
-class StringResponseListener
-{
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 
+class StringResponseListener implements EventSubscriberInterface
+{
+    public function onView(ViewEvent $event): void
+    {
+        $response = $event->getControllerResult();
+
+        if (is_string($response)) {
+            $event->setResponse(new Response($response));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return ['kernel.view' => 'onView'];
+    }
 }
