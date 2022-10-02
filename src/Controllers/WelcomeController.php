322370c896ce0a44affe342a98ec9cb66bf5077d
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 17:36:19 +0300

    feat: udpate welcome request handling

--- src/Controllers/WelcomeController.php
+++ src/Controllers/WelcomeController.php
@@ -3,13 +3,17 @@
 namespace Controllers;
 
 use Models\StoicQuote;
+use Stoic\Facades\View;
 
 class WelcomeController
 {
     public function index(): string
     {
         $stoicQuote = new StoicQuote();
+        $quoteData = $stoicQuote->get();
 
-        return $stoicQuote->get();
+        $template = new View(__DIR__ . '/../resources/views/');
+
+        return $template->render('welcome.view.php', ['quoteData' => $quoteData]);
     }
 }
