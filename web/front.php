d863e1dac1353f798e2088fc923b477c6acf8f12
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 14:09:01 +0300

    feat: connect container builder to front framework

--- web/front.php
+++ web/front.php
@@ -4,9 +4,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
+$container = include __DIR__.'/../src/container.php';
 
-$framework = new Stoic\Framework($routes);
+$request = Request::createFromGlobals();
 
-$framework->handle($request)->send();
+$response = $container->get('framework')->handle($request);
+
+$response->send();
