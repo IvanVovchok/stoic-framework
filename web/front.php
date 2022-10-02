480c28c83c697ba7dfbb4e1ce74b2976589bada4
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 13:23:54 +0300

    feat: add required listeners to front controller

--- web/front.php
+++ web/front.php
@@ -4,32 +4,41 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
+$requestStack = new RequestStack();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
 $dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Stoic\ContentLengthListener());
-$dispatcher->addSubscriber(new Stoic\GoogleListener());
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
 
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
+$errorHandler = function (Symfony\Component\ErrorHandler\Exception\FlattenException $exception) {
+    $msg = 'Something went wrong! ('.$exception->getMessage().')';
 
-$framework = new Stoic\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
-$framework = new HttpKernel\HttpCache\HttpCache(
-    $framework,
-    new HttpKernel\HttpCache\Store(__DIR__.'/../cache'),
-    new HttpKernel\HttpCache\Esi(),
-    ['debug' => true]
+    return new Response($msg, $exception->getStatusCode());
+};
+$dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener($errorHandler));
+
+$listener = new HttpKernel\EventListener\ErrorListener(
+    'Calendar\Controller\ErrorController::exception'
 );
+$dispatcher->addSubscriber($listener);
+
+$framework = new Stoic\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+
+$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
+$dispatcher->addSubscriber(new Stoic\StringResponseListener());
 
 $response = $framework->handle($request);
-
 $response->send();
