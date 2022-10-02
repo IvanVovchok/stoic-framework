b9650db6f0df14259b2c8735571695455bfdef40
Ivan Vovchok <elvovchok@gmail.com>
2022-10-02 15:26:03 +0300

    feat: connect stoic quotes api

--- src/Models/StoicQuote.php
+++ src/Models/StoicQuote.php
@@ -2,7 +2,23 @@
 
 namespace Models;
 
+use GuzzleHttp\Client;
+
 class StoicQuote
 {
+    private string $stoicApiPath = 'https://stoicquotesapi.com/v1/api/quotes/random';
 
+    public function get(): string
+    {
+        $client = new Client([
+            'base_uri' => $this->stoicApiPath,
+            'timeout' => 10.0
+        ]);
+
+        $response = $client->request('GET')->getBody();
+
+        $quoteData = json_decode($response);
+
+        return '"' . $quoteData->body . '" - ' . $quoteData ->author;
+    }
 }
